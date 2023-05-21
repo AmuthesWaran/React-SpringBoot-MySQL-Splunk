b761376964df9f81cd156a5aa6a9b0aec1d730a9
Amutheswaran <amutheswaran@outlook.com>
2023-05-21 20:14:45 +0530

    splunk config done yet to see logs

--- splunk-intergation-springboot-backend/src/main/java/com/ammu/splunkIntergation/controller/EmployeeController.java
+++ splunk-intergation-springboot-backend/src/main/java/com/ammu/splunkIntergation/controller/EmployeeController.java
@@ -2,23 +2,28 @@ package com.ammu.splunkIntergation.controller;
 
 import com.ammu.splunkIntergation.model.Employee;
 import com.ammu.splunkIntergation.service.EmployeeService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.parser.Entity;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
 
+    Logger logger = LogManager.getLogger(EmployeeController.class);
+
     @Autowired
     EmployeeService empServ;
 
     @GetMapping("/")
     public String sayHello(){
+
+        logger.info("Base Url accessed");
         return "Hello World";
     }
     @GetMapping("/{empId}")
--- splunk-intergation-springboot-backend/src/main/java/com/ammu/splunkIntergation/service/EmployeeServiceImpl.java
+++ splunk-intergation-springboot-backend/src/main/java/com/ammu/splunkIntergation/service/EmployeeServiceImpl.java
@@ -2,19 +2,29 @@ package com.ammu.splunkIntergation.service;
 
 import com.ammu.splunkIntergation.model.Employee;
 import com.ammu.splunkIntergation.repository.EmployeeRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements  EmployeeService {
+
+    Logger logger = LogManager.getLogger(EmployeeServiceImpl.class);
+
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    LocalDateTime now = LocalDateTime.now();
 
     @Autowired
     EmployeeRepository empRepo;
 
     @Override
     public Employee addAnEmployee(Employee employee) {
+        logger.info("New Employee is saved. Employee ID is: {}", employee.getEmployeeID());
         return empRepo.save(employee);
     }
 
@@ -29,6 +39,7 @@ public class EmployeeServiceImpl implements  EmployeeService {
 
     @Override
     public List<Employee> getAllEmployee() {
+        logger.info("All employee details accessed at: {}", dtf.format(now));
         return empRepo.findAll();
     }
 
@@ -44,6 +55,8 @@ public class EmployeeServiceImpl implements  EmployeeService {
             deletedEmp = empRepo.findById(employeeId).get();
         }
         empRepo.deleteById(employeeId);
+
+        logger.info("An Employee is deleted. Employee ID: {}", deletedEmp.getEmployeeID());
         return deletedEmp;
     }
 }
